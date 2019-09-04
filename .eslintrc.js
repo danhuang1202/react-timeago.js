02135243e481863e2e952fc6a3c203051054ee59
Dan Huang <danhuang@verizonmedia.com>
2019-09-04 17:41:01 +0800

    prettier .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
     'react-hooks/rules-of-hooks': 'error',
-    "react-hooks/exhaustive-deps": "warn"
+    'react-hooks/exhaustive-deps': 'warn'
   }
 }
